f79e87a779c878726bd938454175d4652819f412
Henry <henryaccounts@yahoo.com>
2022-08-06 10:49:49 +0100

    Updated description

--- commands/help.js
+++ commands/help.js
@@ -11,6 +11,7 @@ module.exports = {
 				{ name: 'dog!weather', value: 'Get Sunny\'s current evolution.' },
 				{ name: 'dog!speak', value: 'Speak to an AI trained with years of Sunny\'s voice samples to get the world\'s most accurate representation of Sunny\'s voice.' },
 				{ name: 'dog!laugh', value: 'Play Sunny\'s laugh (bass boosted) for 6 seconds.' },
+				{ name: 'dog!quiz', value: 'Earn/lose social credit for the CCP by answering basic Chinese history questions.' },
 			)
 			.setTimestamp()
 			.setFooter('Made by mcmakkers#9633');
